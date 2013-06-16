79950d2b2d6cb73a948e1a846df90d271d14ba1a
andrew <otmosina@gmail.com>
2013-06-16 11:28:10 +0400

    delete output

--- view.rb
+++
@@ -1,4 +0,0 @@
-require './using_lib'
-
-out_of_file = YAML.load_file( 'result.yaml' )
-puts  out_of_file.size
--- view_time.rb
+++
@@ -1,6 +0,0 @@
-require './using_lib'
-
-
-a=[]
-YAML.load_file( 'time.yaml' ){|item| puts item}
-puts  a
