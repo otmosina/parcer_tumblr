f94dbb13ed151df07d8aa270aa734054635e8bb8
amosin <a.mosin@inventos.ru>
2013-06-17 19:20:12 +0400

    Отлавливаем исключения из-за ошибок связанных с кодировкой

--- sniffer_notes.rb
+++ sniffer_notes.rb
@@ -13,7 +13,7 @@ total_total_result = []
 out_of_file = YAML.load_file( 'result.yaml' )
 
 start_start = Time.now.to_i
-out_of_file.each do |out_of_file_item|
+out_of_file.each_with_index do |out_of_file_item, index|
 total_result = []	
 current_out_of_file = out_of_file_item	
 link = current_out_of_file[:post_url].match(/50thousand.tumblr.com(.*)/)[1]
@@ -26,13 +26,20 @@ link = link.gsub(/post/,"notes")
 code_for_popup_notes = @result.scan(/tumblrReq.open.*\'(.*)\?/)#(/tumblrReq.open.*\/(.*)\?/)
 
 
-puts "Analize #{current_out_of_file[:post_url]} notes"
+puts "Analize #{current_out_of_file[:post_url]} notes. Index post - #{index}"
+
+File.open( 'analize_post.yaml', 'a' ) do |out|
+  YAML.dump("Analize #{current_out_of_file[:post_url]} notes. Index post - #{index}", out )
+end
+
 threads = []
  
 (current_out_of_file[:note_count].to_i/50).times do
 
 	threads << Thread.new(from_c) do |from_c_th|	
-	
+	  if code_for_popup_notes.empty?
+	  	res_match=[]
+	  else		
 		link_next = code_for_popup_notes[0][0]+"?from_c="+from_c_th.to_s
 		begin
 		  @result=Net::HTTP.get('50thousand.tumblr.com', link_next)
@@ -45,10 +52,11 @@ threads = []
   		end
   
 
-		@result = @result.force_encoding('utf-8').encode
-		res_match = @result.scan(/>(.*)<\/a> reblogged this from <a.*source_tumblelog.*>(.*)<\/a>/)
+		begin @result = @result.force_encoding('utf-8').encode rescue "" end
+		begin res_match = @result.scan(/>(.*)<\/a> reblogged this from <a.*source_tumblelog.*>(.*)<\/a>/) rescue [] end
 
 		res_match
+	  end #else of 	if code_for_popup_notes.empty?
 
 	end
 	from_c = from_c - 60*1
